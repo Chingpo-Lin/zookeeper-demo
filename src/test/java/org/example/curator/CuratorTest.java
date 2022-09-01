370d5019302954ddd48304aef14dfc222567caaf
Chingpo-Lin <m13823601165@163.com>
2022-09-01 12:10:28 -0700

    test for create&get

--- src/test/java/org/example/curator/CuratorTest.java
+++ src/test/java/org/example/curator/CuratorTest.java
@@ -4,6 +4,10 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,10 +15,11 @@ import java.util.List;
 
 public class CuratorTest {
 
+    private CuratorFramework client;
     /**
      * build connect
      */
-    @Test
+    @Before
     public void testConnect() {
         // first method to build connection
 
@@ -23,21 +28,125 @@ public class CuratorTest {
         // 3. connectionTimeoutMs   connection timeout
         // 4. retry policy          retry policy to use
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(3000, 10);
-        CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181",
-                60 * 1000, 15 * 1000, retryPolicy);
-        // start connect
-        client.start();
-        // second method to build connection
-//        CuratorFrameworkFactory.builder().connectString("127.0.0.1:2181")
-//                .sessionTimeoutMs(60 * 1000)
-//                .connectionTimeoutMs(15 * 1000)
-//                .retryPolicy(retryPolicy)
-////                .namespace("itheima") // all operation is set under itheima
-//                .build();
+//        CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181",
+//                60 * 1000, 15 * 1000, retryPolicy);
 //        // start connect
 //        client.start();
+        // second method to build connection
+        client = CuratorFrameworkFactory.builder().connectString("127.0.0.1:2181")
+                .sessionTimeoutMs(60 * 1000)
+                .connectionTimeoutMs(15 * 1000)
+                .retryPolicy(retryPolicy)
+                .namespace("itheima") // all operation is set under itheima
+                .build();
+        // start connect
+        client.start();
     }
 
+    /**
+     * create node (ephemeral, persistent, sequence, data)
+     * 1. basic create
+     */
+    @Test
+    public void testCreate1() throws Exception {
+        // basic create
+        // if not set data, data will default to be current ip
+        String path = client.create().forPath("/app1");
+        System.out.println(path);
+    }
+
+
+    /**
+     * 2. create node with data
+     * @throws Exception
+     */
+    @Test
+    public void testCreate2() throws Exception {
+        // create node with data
+        String path = client.create().forPath("/app2", "hello".getBytes());
+        System.out.println(path);
+    }
+
+    /**
+     * 3. set node type
+     */
+    @Test
+    public void testCreate3() throws Exception {
+        // set node type
+        // default: persistent
+        // ephemeral
+        String path = client.create().withMode(CreateMode.EPHEMERAL).forPath("/app3", "hello".getBytes());
+        System.out.println(path);
+    }
+
+    /**
+     * 4.  create multiple node in different level
+     */
+    @Test
+    public void testCreate4() throws Exception {
+        // create multiple level root
+//        String path = client.create().forPath("/app4");
+//        System.out.println(path);
+//        // if app4 not exist, will throw exception
+//        path = client.create().forPath("/app4/p1");
+//        System.out.println(path);
+        // if parent not exist, will auto create
+        String path = client.create().creatingParentsIfNeeded().forPath("/app4/p1");
+        System.out.println(path);
+    }
+
+    /**
+     * get node data:
+     * 1. get data
+     * @throws Exception
+     */
+    @Test
+    public void testGet1() throws Exception {
+        // 1. get data
+        byte[] data = client.getData().forPath("/app1");
+        System.out.println(new String(data));
+    }
+
+    /**
+     * 2. get children node     ls
+     * @throws Exception
+     */
+    @Test
+    public void testGet2() throws Exception {
+        // 2. get children node (ls)
+        List<String> path = client.getChildren().forPath("/");
+        System.out.println(path); // app1, app2, app4
+        path = client.getChildren().forPath("/app4");
+        System.out.println(path); // p1
+    }
+
+    /**
+     * 3. get node status   ls -s
+     * @throws Exception
+     */
+    @Test
+    public void testGet3() throws Exception {
+        // 3. get status
+        Stat status = new Stat();
+        System.out.println(status);
+        client.getData().storingStatIn(status).forPath("/app1");
+        System.out.println(status);
+    }
+
+
+
+    /**
+     * close
+     */
+    @After
+    public void close() {
+        if (client != null) client.close();
+    }
+
+
+    /**
+     * just curious of sorting order using anonymous
+     */
     @Test
     public void sortList() {
                                         // abc, acd, abcde, abd
